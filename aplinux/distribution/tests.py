a53c761a5f5c208089a953d9ea2adac3c30f2b0e
Adam Terrey <arterrey@gmail.com>
2018-10-25 12:39:06 +1100

    Add google cloud integration test

--- aplinux/distribution/tests.py
+++ aplinux/distribution/tests.py
@@ -4,6 +4,9 @@
 Auto discover tests in this package
 """
 
+import doctest
+import fnmatch
+import os.path
 import pkg_resources
 import unittest
 
@@ -33,4 +36,26 @@ def test_suite(package_name='aplinux.distribution', pattern='*_test.py'):
     suite = test_loader.discover(package_name,
                                  pattern=pattern,
                                  top_level_dir=top_level_dir)
+    return suite
+
+
+def integration_test_suite(package='aplinux.distribution', doctests_path='integration_test', doctest_pattern='*_inttest.rst'):
+    """Create an test suite for integration
+
+    These are heavier weight tests designed  to make sure all the components a re  working togeths
+
+    Args:
+        package_name (str): The package we are interested in loading a test suite for
+        pattern (str): The glob patten used for test discovery
+
+    Returns:
+        TestSuite: The test suite to be used for the test runner
+    """
+    doctest_files = []
+    base_dir = pkg_resources.resource_filename(package, doctests_path)
+    for item_name in pkg_resources.resource_listdir(package, doctests_path):
+        if fnmatch.fnmatch(item_name, doctest_pattern):
+            doctest_file = os.path.join(base_dir, item_name)
+            doctest_files.append(doctest_file)
+    suite = doctest.DocFileSuite(*doctest_files, module_relative=False)
     return suite
