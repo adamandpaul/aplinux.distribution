511e710739155532de1733e8cc5f7a693b8ba801
Adam Terrey <arterrey@gmail.com>
2019-04-22 12:11:49 +0700

    Add generic invoke commands for gce

---
+++ aplinux/distribution/gce_invoke.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+
+from .node_manager import TemporyGCENode
+from invoke import task
+from datetime import datetime
+
+import json
+import logging
+import libcloud
+
+
+logger = logging.getLogger('aplinux.distribution')
+
+
+def get_driver(c):
+    """Return the google cloud driver"""
+    service_account = json.load(open(c.google_cloud.service_account_key_file, 'r'))
+    driver_factory = libcloud.compute.providers.get_driver(libcloud.compute.types.Provider.GCE)
+    return driver_factory(service_account['client_email'],
+                          c.google_cloud.service_account_key_file,
+                          datacenter=c.google_cloud.datacenter,
+                          project=c.google_cloud.project_id,
+                          timeout=300)
+
+
+def new_image_name(c):
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    return f'{c.target_image_prefix}{timestamp}'
+
+
+@task
+def build(c):
+    """Build an image"""
+    import fabfile
+    logger.info('Build a fresh new image.')
+    driver = get_driver(c)
+    kwargs = {**c.google_cloud.node_defaults, **c.build_node}
+    with TemporyGCENode(driver, fabric_config_defaults=c.fabric, **kwargs) as nm:
+        fabfile.build(nm.fabric)
+        nm.stop_and_create_image(new_image_name(c))
+
+
+@task
+def update(c):
+    """Update an image"""
+    import fabfile
+    logger.info('Build an updated image from the most recent image.')
+    driver = get_driver(c)
+    kwargs = {**c.google_cloud.node_defaults, **c.update_node}
+    with TemporyGCENode(driver, fabric_config_defaults=c.fabric, **kwargs) as nm:
+        fabfile.update(nm.fabric)
+        nm.stop_and_create_image(new_image_name(c))
