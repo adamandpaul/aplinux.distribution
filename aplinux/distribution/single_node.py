cdaad5c2c1c3087cbed9f42afb14c9afbee8511a
Adam Terrey <arterrey@gmail.com>
2019-04-29 20:38:41 +1000

    Fix kwargs in in cycle gce single node

--- aplinux/distribution/single_node.py
+++ aplinux/distribution/single_node.py
@@ -14,7 +14,7 @@ def gce_cycle_node(driver, node_name, **kwargs):
 
     # tear down any current nodes
     try:
-        current_node = driver.ex_get_node(node_name, kwargs.location)
+        current_node = driver.ex_get_node(node_name, kwargs['location'])
     except libcloud.common.google.ResourceNotFoundError:
         current_node = None
 
@@ -27,7 +27,7 @@ def gce_cycle_node(driver, node_name, **kwargs):
 
     # Substitute string external_ip to external_ip object
     if isinstance(kwargs.get('external_ip', None), str):
-        kwargs['external_ip'] = driver.ex_get_address(config.external_ip)
+        kwargs['external_ip'] = driver.ex_get_address(kwargs['external_ip'])
 
     # Read in a startup script from the filesystem
     if 'startup_script_path' in kwargs:
